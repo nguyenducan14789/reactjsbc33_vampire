bc3f3955a6946de4f5049bb34480ede539d108b4
Nguyen Duc An <vampire30497@gmail.com>
2022-09-13 16:14:28 +0700

    second commit

---
+++ src/Components/DemoFuncCom.jsx
@@ -0,0 +1,12 @@
+// react class: rcc
+import React, { Component } from 'react'
+
+export default class DemoFuncCom extends Component {
+    // phương thức render chứa giao diện của thẻ <DemoFuncCom/>
+  render() {
+    // nội dung giao diện sẽ chứa trong  lên return render
+    return (
+      <h1>Header Made in ABYSS</h1>
+    )
+  }
+}
